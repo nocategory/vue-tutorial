176fc6d1124f1138799c5d01e4b778f3041542ec
João <joao@joao.codes>
2021-09-04 17:11:08 +0000

    props

---
+++ src/components/Counter.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p v-if="count >= 5"><b>Wohooo! I'm bigger than 5</b></p>
+</template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,14 @@
 <script setup>
-import { ref } from 'vue'
-
+import Counter from './Counter.vue'
 defineProps({
-  msg: String
+  msg: String,
 })
-
-const count = ref(0)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <p>
-    Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
     +
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
@@ -26,7 +22,8 @@ const count = ref(0)
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <Counter />
+
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
@@ -35,6 +32,6 @@ const count = ref(0)
 
 <style scoped>
 a {
-  color: #42b983;
+  color: #6242b9;
 }
 </style>
